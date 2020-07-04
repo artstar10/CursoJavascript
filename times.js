4a98c9d84c25236ef198aa537785a490fde23ae5
Arthur <arthurnevesx@hotmail.com>
2020-07-04 19:05:45 -0300

    Lista de times - enviado via git vscode

---
+++ times.js
@@ -0,0 +1,5 @@
+var libertadores = [ "Cruzeiro", "Atlético-MG", "Internacional", "Grêmio"]
+// loop aqui:
+for(let i=0; i < libertadores.length; i++){
+console.log('Participantes da Copa libertadores da américa: ' + libertadores[i])
+}
